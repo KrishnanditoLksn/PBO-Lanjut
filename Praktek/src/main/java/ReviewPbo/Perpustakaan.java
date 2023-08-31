394d504579ef329bf9f6ae91048462b649609e5c
Gorthdzx <krislaksana123@gmail.com>
2023-08-31 22:28:03 +0700

    merefactor kode agar lebih ringkas

--- Praktek/src/main/java/ReviewPbo/Perpustakaan.java
+++ Praktek/src/main/java/ReviewPbo/Perpustakaan.java
@@ -5,9 +5,13 @@ import java.util.Scanner;
 
 public class Perpustakaan {
     protected ArrayList<Koleksi> koleksiArrayList = new ArrayList<>();
+    protected ArrayList<Peminjam> peminjamArrayList = new ArrayList<>();
     private String namePerpustakaan, Alamat;
 
     public Perpustakaan() {
+        /*
+        TODO()konstruktor kurang rapi
+         */
         koleksiArrayList.add(new Buku("Erlangga", "A223123", "Malin Kundang", "B1", "2004", "120"));
         koleksiArrayList.add(new Buku("Togamas", "A2313123", "Sains", "B2", "2019", "500"));
         koleksiArrayList.add(new Majalah("200", "2020", "Majalah Bobo", "Sinar Dunia", "M1", "Summer 2020", "V-1", "64545456"));
@@ -34,14 +38,14 @@ public class Perpustakaan {
 
     public void showingCollectionList() {
         for (Koleksi koleksi : koleksiArrayList) {
-            if (koleksi instanceof Buku) {
-                System.out.println("\t" + koleksi.getTitle() + "\t" + ((Buku) koleksi).getIsbn() + "\t" + ((Buku) koleksi).getJumlahHalaman());
+            if (koleksi instanceof Buku buku) {
+                System.out.println(buku.getTitle() + "\t" + buku.getIsbn() + "\t" + buku.getJumlahHalaman());
             }
-            if (koleksi instanceof Majalah) {
-                System.out.println("\t" + koleksi.getTitle() + "\t" + ((Majalah) koleksi).getIssn() + ((Majalah) koleksi).getSeries() + "\t" + ((Majalah) koleksi).getVolume());
+            if (koleksi instanceof Majalah majalah) {
+                System.out.println(majalah.getTitle() + "\t" + majalah.getIssn() + majalah.getSeries() + "\t" + majalah.getVolume());
             }
-            if (koleksi instanceof Dvd) {
-                System.out.println("\t" + koleksi.getTitle() + "\t" + ((Dvd) koleksi).getIsbn() + ((Dvd) koleksi).getFormat());
+            if (koleksi instanceof Dvd dvd) {
+                System.out.println(dvd.getTitle() + "\t" + dvd.getIsbn() + dvd.getFormat());
             }
         }
     }
@@ -49,15 +53,14 @@ public class Perpustakaan {
     public String searchingTheKoleksi(String title) {
         for (Koleksi koleksi : koleksiArrayList) {
             if (koleksi.getTitle().equalsIgnoreCase(title)) {
-                if (koleksi instanceof Majalah) {
-                    return "\t" + koleksi.getTitle() + ((Majalah) koleksi).getIssn() + ((Majalah) koleksi).getSeries() + ((Majalah) koleksi).getJumlahHalaman() + ((Majalah) koleksi).getVolume();
+                if (koleksi instanceof Majalah majalah) {
+                    return String.format("%s %s %s %s %s", majalah.getTitle(), majalah.getIssn(), majalah.getSeries(), majalah.getJumlahHalaman(), majalah.getVolume());
                 }
-                if (koleksi instanceof Buku) {
-                    return "\t" + koleksi.getTitle() + ((Buku) koleksi).getIsbn();
+                if (koleksi instanceof Buku buku) {
+                    return String.format("%s %s", buku.getTitle(), buku.getIsbn());
                 }
-                if (koleksi instanceof Dvd) {
-                    return "\t" + koleksi.getTitle() + ((Dvd) koleksi).getIsbn() + ((Dvd) koleksi).getFormat();
-
+                if (koleksi instanceof Dvd dvd) {
+                    return String.format("%s %s %s", dvd.getTitle(), dvd.getIsbn(), dvd.getFormat());
                 } else {
                     return "Not of instance class!!!";
                 }
@@ -69,17 +72,24 @@ public class Perpustakaan {
 
     public String borrowBookByIndex(int index) {
         if ((index < 0 || index <= koleksiArrayList.size())) {
-            for (Koleksi koleksi : koleksiArrayList) {
             /*
-            TODO() pakek get index atau remove
+            TODO() mengembalikan buku dengan method add
              */
-
+            for (Koleksi koleksi : koleksiArrayList) {
                 koleksi = koleksiArrayList.remove(index);
-                return "Anda meminjam buku" + koleksi.getTitle();
 
+                if (koleksi instanceof Buku) {
+                    return "Anda meminjam buku" + koleksi.getTitle() + ((Buku) koleksi).getIsbn();
+                }
+                if (koleksi instanceof Majalah) {
+                    /*return ""*/
+                }
+                if (koleksi instanceof Dvd) {
+
+                }
             }
         }
-        return "Diluar batas index";
+        return null;
     }
 
     public void startMenuShow(Scanner scanner) {
